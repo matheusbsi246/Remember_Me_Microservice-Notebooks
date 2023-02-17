8658449c54b15917255e12424f43caa83451b829
matheusbsi246 <matheusonofre@ufu.br>
2023-02-17 16:08:14 -0300

    hello world

---
+++ src/main/java/com/api/remembermenotebooks/RememberMeNotebooksApplication.java
@@ -0,0 +1,13 @@
+package com.api.remembermenotebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RememberMeNotebooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RememberMeNotebooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/remembermenotebooks/RememberMeNotebooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.remembermenotebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RememberMeNotebooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
